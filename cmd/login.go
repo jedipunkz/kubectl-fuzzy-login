6c7fd7b04e9d21bcde4f1709deb03d7c5d7a24b0
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2023-04-28 23:17:32 +0900

    initial commit

---
+++ cmd/login.go
@@ -0,0 +1,68 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/ktr0731/go-fuzzyfinder"
+	"github.com/spf13/cobra"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+
+	"github.com/jedipunkz/kubecli/internal/kubecli"
+)
+
+// func main() {
+var loginCmd = &cobra.Command{
+	Use:   "login",
+	Short: "Login to a Kubernetes Pod",
+	Run: func(cmd *cobra.Command, args []string) {
+		login()
+	},
+}
+
+func login() {
+	kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube", "config")
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		fmt.Printf("Error building kubeconfig: %v\n", err)
+		os.Exit(1)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Printf("Error creating clientset: %v\n", err)
+		os.Exit(1)
+	}
+
+	podGetter := &kubecli.PodGetterImpl{}
+
+	podList, err := podGetter.GetPods(clientset)
+	if err != nil {
+		fmt.Printf("Error getting pods: %v\n", err)
+		os.Exit(1)
+	}
+
+	index, err := fuzzyfinder.Find(podList, func(i int) string {
+		return podList[i].Name
+	})
+
+	if err != nil {
+		fmt.Printf("Error finding pod: %v\n", err)
+		os.Exit(1)
+	}
+
+	selectedPod := podList[index]
+	fmt.Printf("Logging into pod %s in namespace %s...\n", selectedPod.Name, selectedPod.Namespace)
+	podExecutor := &kubecli.PodExecutorImpl{}
+	if err := podExecutor.ExecInPod(clientset, config, selectedPod.Name, selectedPod.Namespace); err != nil {
+		fmt.Printf("Error executing command in pod: %v\n", err)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(loginCmd)
+}
