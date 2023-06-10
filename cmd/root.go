68679b6728016a6256ddd0a717bef37f09e9bdde
Tomokazu HIRAI <tomokazu.hirai@gmail.com>
2023-06-11 01:17:15 +0900

    fix

--- cmd/root.go
+++ cmd/root.go
@@ -1,27 +1,114 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 
+	"github.com/fatih/color"
+	"github.com/ktr0731/go-fuzzyfinder"
 	"github.com/spf13/cobra"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+
+	mykube "github.com/jedipunkz/kubectl-login-pod/internal/kubernetes"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "kubecli",
-	Short: "Kubernetes CLI",
-	Long: `kubecli provides Kubernetes Client feature not provided by kubectl.
-This app made for my learning purposes:
+var namespace string
 
-At first, This CLI Provides to login a pod on each namespaces`,
-}
+var RootCmd = &cobra.Command{
+	Use:   "kubectl login pod",
+	Short: "kubectl plugin to login to a kubernetes pod",
+	Run: func(cmd *cobra.Command, args []string) {
 
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
+		kubeconfigPath := filepath.Join(homedir.HomeDir(), ".kube", "config")
+
+		config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+		if err != nil {
+			fmt.Printf("Error building kubeconfig: %v\n", err)
+			os.Exit(1)
+		}
+
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			fmt.Printf("Error creating clientset: %v\n", err)
+			os.Exit(1)
+		}
+
+		podGetter := &mykube.PodGetterImpl{}
+
+		podList, err := podGetter.GetPods(clientset, namespace)
+		if err != nil {
+			fmt.Printf("Error getting pods: %v\n", err)
+			os.Exit(1)
+		}
+
+		podIndex, err := fuzzyfinder.Find(
+			podList,
+			func(i int) string {
+				return podList[i].Name
+			},
+			fuzzyfinder.WithPreviewWindow(func(i, w, h int) string {
+				if i == -1 {
+					return ""
+				}
+				return fmt.Sprintf(
+					"%s: %s\n%s: %s\n%s: %s\n%s: %s\n",
+					color.CyanString("Name"), podList[i].Name,
+					color.MagentaString("Namespace"), podList[i].Namespace,
+					color.YellowString("Creating Timestamp"), podList[i].CreationTimestamp,
+					color.RedString("UUID"), podList[i].UID,
+				)
+			}))
+
+		if err != nil {
+			fmt.Printf("Error finding pod: %v\n", err)
+			os.Exit(1)
+		}
+
+		selectedPod := podList[podIndex]
+
+		containerGetter := &mykube.ContainerGetterImpl{}
+		containerList, err := containerGetter.GetContainers(clientset, selectedPod.Name, selectedPod.Namespace)
+
+		if err != nil {
+			fmt.Printf("Error getting containers: %v\n", err)
+			os.Exit(1)
+		}
+
+		containerIndex, err := fuzzyfinder.Find(
+			containerList,
+			func(i int) string {
+				return containerList[i].Name
+			},
+			fuzzyfinder.WithPreviewWindow(func(i, w, h int) string {
+				if i == -1 {
+					return ""
+				}
+				return fmt.Sprintf(
+					"%s: %s\n%s: %s\n",
+					color.CyanString("Name"), containerList[i].Name,
+					color.MagentaString("Image"), containerList[i].Image,
+				)
+			}))
+
+		if err != nil {
+			fmt.Printf("Error finding container: %v\n", err)
+			os.Exit(1)
+		}
+
+		selectedContainer := containerList[containerIndex]
+
+		fmt.Printf("Logging into container %s in pod %s in namespace %s...\n", selectedContainer.Name, selectedPod.Name, selectedPod.Namespace)
+
+		podExecutor := &mykube.PodExecutorImpl{}
+		if err := podExecutor.ExecInPod(clientset, config, selectedPod.Name, selectedPod.Namespace, selectedContainer.Name); err != nil {
+			fmt.Printf("Error executing command in container: %v\n", err)
+		}
+	},
 }
 
 func init() {
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	RootCmd.Flags().StringVarP(&namespace, "namespace", "n", "", "Kubernetes namespace")
 }
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
-import "github.com/jedipunkz/kubecli/cmd"
+import "github.com/jedipunkz/kubectl-login-pod/cmd"
 
 func main() {
-	cmd.Execute()
+	cmd.RootCmd.Execute()
 }
